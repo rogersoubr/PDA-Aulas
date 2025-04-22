9c2f4282404422873dd6b8e30eb25ea03c0c5524
roger <rogersoubrand1a@gmail.com>
2025-04-22 17:18:06 -0300

    aprofundando a A9 node.js

--- M4/A9-1_animais/src/routes/Animais.route.js
+++ M4/A9-1_animais/src/routes/Animais.route.js
@@ -6,6 +6,6 @@ const animais = Router();
 
 animais.get("/", AnimaisController.getAnimais); //vai nessa rota
 
-animais.post("/cadastra", AnimaisController.criarAnimais); //vai nessa rota, onde abre o método POST que leva ao método que cria
+animais.post("/cadastra", sAnimaisController.criarAnimais); //vai nessa rota, onde abre o método POST que leva ao método que cria
 
 export default animais; //exporta a intancia
